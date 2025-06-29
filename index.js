e9bb523f61ca412126e9e0e6da6781661a17de2d
krishkumarchhabra <123287035+KrishKuChhabra@users.noreply.github.com>
2025-06-29 15:33:37 +0530

    issue solved for production

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const dotenv = require("dotenv");
 const express = require("express");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
-const authRoutes = require("./routes/UserRoutes")
-const walletAccountRoutes = require("./routes/walletAccountRoute")
-const cardRoutes = require("./routes/CardRoutes")
-const TransactionRoutes = require("./routes/TransactionRoutes")
-const BillsEmiRoutes = require("./routes/BillsEmiRoutes")
-const SubscriptionRoutes = require("./routes/SubscriptionRoutes")
+const authRoutes = require("./routes/UserRoutes");
+const walletAccountRoutes = require("./routes/walletAccountRoute");
+const cardRoutes = require("./routes/CardRoutes");
+const TransactionRoutes = require("./routes/TransactionRoutes");
+const BillsEmiRoutes = require("./routes/BillsEmiRoutes");
+const SubscriptionRoutes = require("./routes/SubscriptionRoutes");
 require("./utils/autoPay");
 
-const cors = require('cors')
+const cors = require("cors");
+
 dotenv.config({
-  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env.development"
+  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env.development",
 });
 
 connectDB();
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(express.json())
-app.use(cookieParser())
-
- const corsOptions = {
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token'],
-  credentials: false
-}; 
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-access-token"],
+  credentials: false,
+};
 
 app.use(cors(corsOptions));
 
-app.use("/auth" , authRoutes)
-app.use("/wallet" , walletAccountRoutes)
-app.use("/card" , cardRoutes)
-app.use("/transaction" , TransactionRoutes)
-app.use("/billemi" , BillsEmiRoutes)
-app.use("/subscription"  ,SubscriptionRoutes )
+app.use("/auth", authRoutes);
+app.use("/wallet", walletAccountRoutes);
+app.use("/card", cardRoutes);
+app.use("/transaction", TransactionRoutes);
+app.use("/billemi", BillsEmiRoutes);
+app.use("/subscription", SubscriptionRoutes);
 
 app.get("/", (req, res) => {
   res.send(`
@@ -68,10 +68,13 @@ app.get("/", (req, res) => {
       <body>
         <div>
           <h1>🚀 FinanceAI Backend</h1>
-          <p>Server is running successfully on <strong>PORT ${process.env.PORT || 5000}</strong></p>
+          <p>Server is running successfully on <strong>Vercel Function</strong></p>
           <p>Made with ❤️ by Krish</p>
         </div>
       </body>
     </html>
   `);
 });
+
+
+module.exports = app;
